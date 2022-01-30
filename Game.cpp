31b7977da4d945c3d323161370b7535d30b70e7c
Vadim <vadkostinwork@gmail.com>
2022-01-30 15:40:33 +0200

    Add the game

--- Game.cpp
+++ Game.cpp
@@ -43,7 +43,44 @@ int main(){
         if(level==1 || level==2 || level==3 || level==4) break;
     }
 
-    cout<<"\n\nYou will have unlimited amount of attempts to guess the number\nDepending on this amount of attempts you`ll have different rank:"<<endl<<endl;
-    cout<<"If you guess on the first try, you will get the hieghst rank\nIf you guess from 2 to 10 attempt - normal rank\nFrom 10 to 20 - medium rank\nAnd if you guess the number on more then 20 attempts you will get low rank"<< endl;
+    cout<<"\n\nYou will have unlimited amount of attempts to guess the number";
+    Sleep(3000);
+    cout<<"\nDepending on this amount of attempts you`ll have different rank:"<<endl<<endl;
+    Sleep(3000);
+    cout<<"If you guess on the first try, you will get the highest rank"<<endl;
+    Sleep(3000);
+    cout<<"If you guess from 2 to 10 attempt - normal rank"<<endl;
+    Sleep(2000);
+    cout<<"From 11 to 20 - medium rank"<<endl;
+    Sleep(2000);
+    cout<<"And if you guess the number on more then 20 attempts you will get low rank"<< endl;
+    Sleep(3000);
+
+    cout<<"\nStart the Game"<<endl;
+    int count=0,userNumber;
+    while (true)
+    {
+        count++;
+        cout<<"\nGuess the number: "; cin>>userNumber;
+        if (userNumber>number)
+            cout<<"\nLess"<<endl;
+        else if(userNumber<number)
+            cout<<"\nMore"<<endl;
+        else{
+            cout<<"\nYou win!!!"<<endl;
+            break;
+        }
+    }
+
+    cout<<"You guessed on the "<<count<<" try!"<<endl;
+    if (count==1)
+        cout<<"You got the highest rank!!! Knockout!"<<endl;
+    else if(count>=2 && count<=10)
+        cout<<"You got the normal rank!!! Nice try!"<<endl;
+    else if(count>=11 && count<=20)
+        cout<<"You got the medium rank! You worked hurd!"<<endl;
+    else
+        cout<<"You got the low rank! Better luck next time!"<<endl;
+    
     system("pause");
 }
